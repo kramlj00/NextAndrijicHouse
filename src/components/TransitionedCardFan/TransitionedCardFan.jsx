7d96de4cb15555c17c34b73c59239952bf733869
Kristina Ramljak <kristinaramljak5@gmail.com>
2023-11-19 12:07:47 +0100

    Updated transition cardfan

--- src/components/TransitionedCardFan/TransitionedCardFan.jsx
+++ src/components/TransitionedCardFan/TransitionedCardFan.jsx
@@ -4,24 +4,24 @@ import { useInView } from 'react-intersection-observer';
 import styles from './cardFan.module.scss';
 
 const TransitionedCardFan = () => {
-  const { ref, inView, entry } = useInView({
-    /* Optional options */
+  const { ref, inView } = useInView({
     threshold: 0,
   });
   const [isSpread, setIsSpread] = useState(true);
 
   useEffect(() => {
+    inView && setIsSpread(true);
+    const timeout = inView ? 5000 : 0;
     setTimeout(() => {
       setIsSpread(false);
-    }, [5000]);
-  }, []);
+    }, [timeout]);
+  }, [inView]);
 
   return (
     <div>
       <div
         className={inView && isSpread ? styles.spreadCardfan : styles.cardfan}
-        ref={ref}
-      >
+        ref={ref}>
         <div className={`${styles.cardfanImage} ${styles.pic3}`}>
           <Image
             src="https://res.cloudinary.com/kristina1950/image/upload/v1659702042/rooms/room17-image_ir36t2.jpg"
@@ -50,8 +50,7 @@ const TransitionedCardFan = () => {
       <svg
         className={styles.svgImage}
         version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-      >
+        xmlns="http://www.w3.org/2000/svg">
         <filter id="blur">
           <feGaussianBlur stdDeviation="3" />
         </filter>
@@ -59,8 +58,7 @@ const TransitionedCardFan = () => {
       <svg
         className={styles.svgImage}
         version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-      >
+        xmlns="http://www.w3.org/2000/svg">
         <filter id="greyscale">
           <feColorMatrix
             type="matrix"
@@ -74,8 +72,7 @@ const TransitionedCardFan = () => {
       <svg
         className={styles.svgImage}
         version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-      >
+        xmlns="http://www.w3.org/2000/svg">
         <filter id="sepia">
           <feColorMatrix
             values="0.14 0.45 0.05 0 0
