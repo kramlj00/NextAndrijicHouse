b742a33c4fc44c591f70c4e51ce8508e95c03692
kramlj00 <kristinaramljak5@gmail.com>
2025-10-10 16:48:16 +0200

    Added location

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -1,9 +1,9 @@
-import { LocateIcon } from 'lucide-react';
-import SectionTitle from '../SectionTitle';
+import { MailIcon, MapPinIcon, PhoneIcon } from 'lucide-react';
 import styles from './location.module.scss';
 import { useRouter } from 'next/router';
 import en from '@locales/en';
 import hr from '@locales/hr';
+import { PHONE_NUMBER, EMAIL } from '@shared/constants';
 
 const Location = () => {
   const router = useRouter();
@@ -14,24 +14,48 @@ const Location = () => {
     <div
       data-aos="fade-up"
       data-aos-duration="1000"
-      className={styles.locationContainer}>
-      <SectionTitle
-        title={t.locationTitle}
-        subTitle={t.locationSubtitle}
-        sectionName={t.locationSectionName}
-        icon={<LocateIcon color="#1277b0" size={20} />}
-      />
-      <div className={styles.locationMapContainer}>
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2921.753562941688!2d16.7132849763236!3d42.920238099527104!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x134a720c5f6330cf%3A0x810796f65a20ec98!2sAndriji%C4%87%20House!5e0!3m2!1sen!2shr!4v1760079476176!5m2!1sen!2shr&t=m&z=15"
-          className={styles.locationMap}
-          style={{
-            border: 0,
-            // filter: 'invert(95%) hue-rotate(180deg)',
-          }}
-          loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
-        />
+      className={styles.container}>
+      <div className={styles.contactUsContainer}>
+        <div className={styles.locationMapContainer}>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2921.753562941688!2d16.7132849763236!3d42.920238099527104!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x134a720c5f6330cf%3A0x810796f65a20ec98!2sAndriji%C4%87%20House!5e0!3m2!1sen!2shr!4v1760079476176!5m2!1sen!2shr&t=m&z=15"
+            className={styles.locationMap}
+            style={{
+              border: 0,
+              // filter: 'invert(95%) hue-rotate(180deg)',
+            }}
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          />
+        </div>
+        <div className={styles.contactUsDetails}>
+          <div>
+            <h1 className={styles.contactUsTitle}>{t.contactUs}</h1>
+            <p className={styles.contactUsDescription}>
+              {t.contactDescription}
+            </p>
+          </div>
+          <div className={styles.contactUsDetails}>
+            <div className={styles.contactUsDetailContainer}>
+              <MapPinIcon size={20} />
+              <p className={styles.contactUsDetail}>{t.address}</p>
+            </div>
+            <div className={styles.contactUsDetailContainer}>
+              <PhoneIcon size={20} />
+              <a
+                href={`tel:${PHONE_NUMBER.value}`}
+                className={styles.contactUsDetail}>
+                {PHONE_NUMBER.label}
+              </a>
+            </div>
+            <div className={styles.contactUsDetailContainer}>
+              <MailIcon size={20} />
+              <a href={`mailto:${EMAIL}`} className={styles.contactUsDetail}>
+                {EMAIL}
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/shared/constants/personal.const.js
@@ -0,0 +1,2 @@
+export const PHONE_NUMBER = { value: '+385981816135', label: '+385 98 181 6135' };
+export const EMAIL = 'info@andrijichouse.com';
