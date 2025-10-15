c4afc53bab560261ddf127d936a5307ad0ad7952
kramlj00 <kristinaramljak5@gmail.com>
2025-10-15 15:29:31 +0200

    Added lenis

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -4,8 +4,25 @@ import ExteriorImagesGallery from './ExteriorImagesGallery';
 import InteriorImagesGallery from './InteriorImagesGallery';
 import Testimonials from '../../components/Testimonials';
 import Location from '../../components/Location';
+import { useEffect } from 'react';
 
 const Home = () => {
+  useEffect(() => {
+    const initLenis = async () => {
+      const Lenis = (await import('lenis')).default;
+
+      const lenis = new Lenis({
+        autoRaf: true,
+      });
+
+      return () => {
+        lenis.destroy();
+      };
+    };
+
+    initLenis();
+  }, []);
+
   return (
     <>
       <MainImage
