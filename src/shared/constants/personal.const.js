9e29712d5fdcaa09599d9118e63a5c3ec58fef7f
kramlj00 <kristinaramljak5@gmail.com>
2025-10-30 10:36:42 +0100

    Updated email

--- src/shared/constants/personal.const.js
+++ src/shared/constants/personal.const.js
@@ -1,2 +1,5 @@
-export const PHONE_NUMBER = { value: '+385981816135', label: '+385 98 181 6135' };
-export const EMAIL = 'info@andrijichouse.com';
+export const PHONE_NUMBER = {
+  value: '+385981816135',
+  label: '+385 98 181 6135',
+};
+export const EMAIL = 'tonci.andrijic@gmail.com';
